15c054fee1bb48c30304c18aa38dcf194c0916df
Nathaniel <ndbutter@geneva.edu>
2013-02-05 21:31:24 -0500

    initial commit: server built

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marcello::Application.config.secret_token = '2daddbdd4349414a2bdd1f162946638f57da9083836c13f43d09f9a76aed588bea597daf8360083fcad3dc31cb5d4f3a7a6ad2232ed2f3b7c3018bf48a13760e'
