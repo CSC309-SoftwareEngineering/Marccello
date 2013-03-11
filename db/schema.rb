161126c485db6b937c8026d846b01da1f5c772e1
Nathaniel <empollondefisica@gmail.com>
2013-03-10 22:22:17 -0400

    Added drop-down to display courses after selecting a major. Very crude.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130227011434) do
+ActiveRecord::Schema.define(:version => 20130311021138) do
 
   create_table "courses", :force => true do |t|
     t.string   "department"
     t.integer  "course_number"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.string   "name"
   end
 
   create_table "majors", :force => true do |t|
