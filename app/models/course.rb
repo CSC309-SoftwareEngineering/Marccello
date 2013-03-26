87cda20228765af7b48f18a58f7c7458bca50abe
Alex Grover <alex@alex-grover.com>
2013-03-26 12:13:58 -0400

    Added validation of uniqueness to the Course and Major Models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
+  validates_uniqueness_of :name, :case_sensitive => false, :message => " - This Course already exists."
   attr_accessible :course_number, :department, :name
   has_many :requirements
   has_many :majors, :through => :requirements
--- app/models/major.rb
+++ app/models/major.rb
@@ -1,4 +1,5 @@
 class Major < ActiveRecord::Base
+  validates_uniqueness_of :name, :case_sensitive => false, :message => " - This Major already exists."
   attr_accessible :name
   has_many :requirements
   has_many :courses, :through => :requirements
